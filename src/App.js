574c8fc6b37dee6a8c4b8de7b00c53776e3ba487
Umar Haroon <umha9535@colorado.edu>
2024-01-23 00:00:53 -0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './Pages/Home';
+import Membership from './Pages/Membership';
+import About from './Pages/About';
+import Contact from './Pages/Contact';
+import WeightBalance from './Pages/WeightBalance';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+  {
+    path: "/membership",
+    element: <Membership />,
+  },
+  {
+    path: "/wb",
+    element: <WeightBalance />,
+  },
+]);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RouterProvider router={router}/>
   );
 }
 
---
+++ src/Pages/Contact.js
@@ -0,0 +1,24 @@
+import '../App.css';
+
+function Contact() {
+    return (
+        <div id="ui-container" className="h-auto min-h-screen grid justify-center items-center">
+        <header className="fill w-full bg-white shadow-md px-4 py-2">
+        <nav>
+          <h1 className="text-2xl font-bold">Boulder Flying Club</h1>
+          <ul className="flex space-x-4">
+            <li><a href="/" className="hover:bg-gray-200 px-2 py-1 rounded">Home</a></li>
+            <li><a href="membership" className="hover:bg-gray-200 px-2 py-1 rounded">Memberships</a></li>
+            <li><a href="contact" className="hover:bg-gray-200 px-2 py-1 rounded">Contact</a></li>
+            <li><a href="about" className="hover:bg-gray-200 px-2 py-1 rounded">About</a></li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+      {/* <h1 className="text-2xl font-bold">Memberships</h1> */}
+      </main>
+      
+      </div>
+    )
+}
+export default Contact;  
